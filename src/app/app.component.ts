c63dec16220d62456a80a32288de03b2ec7ac01b
smnsohail <1smnsohail@gmail.com>
2025-02-08 19:26:37 +0530

    correction in loading abt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './main/header/header.component';
 import { FooterComponent } from './main/footer/footer.component';
 
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet,HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Sohail M Nadaf';
-
-  // ngOnInit(){
-  //   this.changeTitle();
-  // }
-  // changeTitle(){
-  //   this.title = 'Sohail M Nadaf - Angular Developer';
-  // }
+  title = 'first-app';
 }
